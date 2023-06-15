6a7dde8473e64defce12e98c258a60afdff7df5c
Deaa Aldin Alawad <108593938+deaamanger@users.noreply.github.com>
2023-06-16 00:43:07 +0200

    Update index.js
    EDTING INDEX

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: false,
-    userDataDir: "./tmp"
+    userDataDir: "your DATA FOLDER PATH NAME LIKE ./tmp"
   });
   const page = await browser.newPage();
 
-  await page.goto('https://www.amazon.com/s?k=amazonbasics&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=18X3EL2VAGQSY&sprefix=amazonbasics%2Caps%2C220&ref=nb_sb_noss_2',{timeout:3000000});
+  await page.goto(' let your Link There ',{timeout:3000000});
 
-  const handels = await page.$$('.s-main-slot .s-result-list .s-search-results .sg-row');
+  const handels = await page.$$('write Html Classes here like the Big-div ( .s-search-results .sg-row)');
   for(const handel of handels){
-    const singleweet = await page.evaluate(el.querySelector("h2 > a > span").textContent, handel)
+    const singleweet = await page.evaluate(el.querySelector(" write querySelector for Html Elemant // children // Like  h2 > a > span").textContent, handel)
     console.log(singleweet)
   }
-  //await page.pdf({path: "deaa.pdf"});
+  // Some testing and export pdf file and screenshot //
+  // just try and enjoy //
+  
+  await page.pdf({path: "deaa.pdf"});
 
-  // Set screen size
+
   
   // Type into search box
   {/*
@@ -40,6 +43,6 @@ const puppeteer = require('puppeteer');
  
 
   */}
-  //await page.screenshot({ path: "deaa6.png"})
+  await page.screenshot({ path: "deaa6.png"})
   //await browser.close();
 })();
